00988659f2fb00d568239cbcaf77ce07dab52989
shaun.bolak <shaun.bolak@everbridge.com>
2022-11-16 18:36:04 -0500

    sass xxl query created.  Overall design still in progress...

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,7 +1,7 @@
 import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, copyOne, copyTwo, copyThree } = category;
     return (
         <div className="category-container">
             <div className="category-bg-element">
@@ -11,10 +11,19 @@ const CategoryItem = ({ category }) => {
                         backgroundImage: `url(${imageUrl})`,
                     }}
                 />
+                <div className={`${title}`}>
+                    <span>{copyOne}</span>
+                    <span>{copyTwo}</span>
+                    <span>{copyThree}</span>
+                    <div className="cta">
+                        <a className="boxy" href={imageUrl}>
+                            Shop Now
+                        </a>
+                    </div>
+                </div>
             </div>
 
-            {/* <div className="category-spacer"></div> */}
-            {/* <div className="category-body-container">
+            {/* <div className={`category-body-container ${title}`}>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div> */}
--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -5,30 +5,45 @@ const categories = [
     {
         id: 1,
         title: "new",
+        copyOne: "So Fresh",
+        copyTwo: "And So",
+        copyThree: "Clean Clean",
         imageUrl:
             "https://images.pexels.com/photos/605206/pexels-photo-605206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
         id: 2,
         title: "womens",
+        copyOne: "The Ultimate",
+        copyTwo: "Desk-To-Party",
+        copyThree: "Outfits",
         imageUrl:
             "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
         id: 3,
         title: "mens",
+        copyOne: "New Styles",
+        copyTwo: "to",
+        copyThree: "Make You Smile",
         imageUrl:
             "https://images.pexels.com/photos/3206079/pexels-photo-3206079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
         id: 4,
         title: "bags",
+        copyOne: "Bags That Refine",
+        copyTwo: "Styles",
+        copyThree: "That Shine",
         imageUrl:
             "https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
         id: 5,
         title: "accessories",
+        copyOne: "Accessories",
+        copyTwo: "To",
+        copyThree: "Murder",
         imageUrl:
             "https://images.pexels.com/photos/1639729/pexels-photo-1639729.jpeg?auto=compress&cs=tinysrgb&w=600&h=400&dpr=1",
     },
