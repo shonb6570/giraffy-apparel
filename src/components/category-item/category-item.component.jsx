3f9748b7f33e34999cb5aba388af48e6a3a860bc
shaun bolak <shaunbolak@gmail.com>
2023-06-09 23:06:47 -0400

    dynamic link needed to be adjusted for the server

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,34 +1,35 @@
 import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
-    const { imageUrl, title, copyOne, copyTwo, copyThree, copyFour } = category;
-    return (
-        <div className="category-wrapper">
-            <div className="category-bg-element">
-                <div
-                    className="category-img"
-                    style={{
-                        backgroundImage: `url(${imageUrl})`,
-                    }}
-                />
-                <div className={`${title}`}>
-                    <h2>{copyOne}</h2>
-                    <h2>{copyTwo}</h2>
-                    <h2>{copyThree}</h2>
-                    <div className="category-paragraph-wrapper">
-                        <span>{copyFour}</span>
-                    </div>
-                    <div className="cta">
-                        <a
-                            className="boxy"
-                            href={`http://localhost:3000/shop/${title}`}>
-                            Shop Now
-                        </a>
-                    </div>
-                </div>
-            </div>
+  const { imageUrl, title, copyOne, copyTwo, copyThree, copyFour } = category;
+  return (
+    <div className="category-wrapper">
+      <div className="category-bg-element">
+        <div
+          className="category-img"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        />
+        <div className={`${title}`}>
+          <h2>{copyOne}</h2>
+          <h2>{copyTwo}</h2>
+          <h2>{copyThree}</h2>
+          <div className="category-paragraph-wrapper">
+            <span>{copyFour}</span>
+          </div>
+          <div className="cta">
+            <a
+              className="boxy"
+              href={`http://giraffyapparel.shaunbolak.com/shop/${title}`}
+            >
+              Shop Now
+            </a>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CategoryItem;
