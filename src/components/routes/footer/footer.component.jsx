e62dbb61ed77fa55e8d3c26177dca5a05958691d
shaun bolak <shaunbolak@gmail.com>
2023-06-09 22:56:27 -0400

    updated links to work correctly.  sass edits.  updated dependancies

--- src/components/routes/footer/footer.component.jsx
+++ src/components/routes/footer/footer.component.jsx
@@ -3,73 +3,43 @@ import "./footer.styles.scss";
 const year = new Date().getFullYear();
 
 const Footer = () => {
-    return (
-        <>
-            <div className="footer-container">
-                <div className="footer-content-wrapper">
-                    <div className="footer-bg"></div>
-                    <div className="giraffy-icon"></div>
-                    <div className="footer-logo"></div>
-                    <div className="footer-links-container">
-                        <div className="footer-brand-title">
-                            <p>iraffy Apparel Co.</p>
-                        </div>
-                        <div className="footer-links-wrapper">
-                            <div className="footer-nav-links">
-                                <span className="footer-nav-link">Contact</span>
-                                <span className="footer-nav-link">Support</span>
-                                <span className="footer-nav-link">About</span>
-                                <span className="footer-nav-link">Careers</span>
-                            </div>
-                            <hr className="footer-hr-verticle" />
-                            <div className="social-links">
-                                <div>
-                                    <a href="www.twitter.com">
-                                        <div
-                                            alt="twitter icon"
-                                            className="social-icon"></div>
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="www.twitter.com">
-                                        <div
-                                            alt="twitter icon"
-                                            className="social-icon"></div>
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="www.twitter.com">
-                                        <div
-                                            alt="twitter icon"
-                                            className="social-icon"></div>
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="www.twitter.com">
-                                        <div
-                                            alt="twitter icon"
-                                            className="social-icon"></div>
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="www.twitter.com">
-                                        <div
-                                            alt="twitter icon"
-                                            className="social-icon linkedin"></div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="footer-container">
+        <div className="footer-content-wrapper">
+          <div className="footer-bg"></div>
+          <div className="giraffy-icon"></div>
+          <div className="footer-logo"></div>
+          <div className="footer-links-container">
+            <div className="footer-brand-title">
+              <p>iraffy Apparel Co.</p>
             </div>
-            <div className="footer-copyright-container">
-                <div className="footer-copyright">
-                    <p>&#169; Giraffy Apparel Co. &nbsp; {year}</p>
+            <div className="footer-links-wrapper">
+              <div className="footer-nav-links">
+                <span className="footer-nav-link">Contact</span>
+                <span className="footer-nav-link">Support</span>
+                <span className="footer-nav-link">About</span>
+                <span className="footer-nav-link">Careers</span>
+              </div>
+              <hr className="footer-hr-verticle" />
+              <div className="social-links">
+                <div>
+                  <a href="www.twitter.com">
+                    <div alt="twitter icon" className="social-icon"></div>
+                  </a>
                 </div>
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright-container">
+        <div className="footer-copyright">
+          <p>&#169; Giraffy Apparel Co. &nbsp; {year}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -2,62 +2,60 @@ import CategoryDirectory from "../../../components/category-directory/category-d
 import "./home.styles.scss";
 
 const categories = [
-    {
-        id: 1,
-        title: "new",
-        copyOne: "So Fresh",
-        copyTwo: "And So",
-        copyThree: "Clean Clean",
-        copyFour: "Oh yeah.  New styles are in!",
-        imageUrl:
-            "https://images.pexels.com/photos/605206/pexels-photo-605206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-        id: 2,
-        title: "womens",
-        copyOne: "The Ultimate",
-        copyTwo: "Desk-To-Party",
-        copyThree: "Outfits",
-        copyFour:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        imageUrl:
-            "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-        id: 3,
-        title: "mens",
-        copyOne: "New Styles",
-        copyTwo: "to Make",
-        copyThree: "You Smile",
-        copyFour: "Lorem ipsum dolor sit amet. consectetur adipiscing elit.",
-        imageUrl:
-            "https://images.pexels.com/photos/3206079/pexels-photo-3206079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-        id: 4,
-        title: "bags",
-        copyOne: "Bags That Refine",
-        copyTwo: "Styles",
-        copyThree: "That Shine",
-        copyFour:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
-        imageUrl:
-            "https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-        id: 5,
-        title: "accessories",
-        copyOne: "Accessories",
-        copyTwo: "To",
-        copyThree: "Murder",
-        copyFour: "Lorem ipsum dolor sit amet, consectetur.",
-        imageUrl:
-            "https://images.pexels.com/photos/1639729/pexels-photo-1639729.jpeg?auto=compress&cs=tinysrgb&w=600&h=400&dpr=1",
-    },
+  {
+    id: 1,
+    title: "jackets",
+    copyOne: "So Fresh",
+    copyTwo: "And So",
+    copyThree: "Clean Clean",
+    copyFour: "Oh yeah.  New jackets are in!",
+    imageUrl:
+      "https://images.pexels.com/photos/605206/pexels-photo-605206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    title: "womens",
+    copyOne: "The Ultimate",
+    copyTwo: "Desk-To-Party",
+    copyThree: "Outfits",
+    copyFour: "We got you covered.  So what are you waiting for?",
+    imageUrl:
+      "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 3,
+    title: "mens",
+    copyOne: "New Styles",
+    copyTwo: "to Make",
+    copyThree: "You Smile",
+    copyFour: "All of the best gear to get you stylin'.",
+    imageUrl:
+      "https://images.pexels.com/photos/3206079/pexels-photo-3206079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    title: "hats",
+    copyOne: "Hats That Refine",
+    copyTwo: "Styles",
+    copyThree: "That Shine",
+    copyFour: "Check out our new hats for the season!",
+    imageUrl:
+      "https://images.pexels.com/photos/1878821/pexels-photo-1878821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 5,
+    title: "sneakers",
+    copyOne: "Accessories",
+    copyTwo: "To",
+    copyThree: "Murder",
+    copyFour: "The best sneakers in the game!",
+    imageUrl:
+      "https://images.pexels.com/photos/1639729/pexels-photo-1639729.jpeg?auto=compress&cs=tinysrgb&w=600&h=400&dpr=1",
+  },
 ];
 
 const Home = () => {
-    return <CategoryDirectory categories={categories} />;
+  return <CategoryDirectory categories={categories} />;
 };
 
 export default Home;
