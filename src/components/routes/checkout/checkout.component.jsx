35c206988d24f804c10b84766cfbc7e711c875f7
shaun bolak <shaunbolak@gmail.com>
2023-06-19 21:41:30 -0400

    updated mobile styles

--- src/components/routes/checkout/checkout.component.jsx
+++ src/components/routes/checkout/checkout.component.jsx
@@ -4,26 +4,26 @@ import CheckoutItem from "../../checkout-item/checkout-item.component";
 import "./checkout.styles.scss";
 
 const Cart = () => {
-    const { cartItems, cartTotal } = useContext(CartContext);
-    return (
-        <>
-            <div className="container checkout-wrapper">
-                <div className="checkout-container">
-                    <span>Product</span>
-                    <span>Description</span>
-                    <span>Quantity</span>
-                    <span>Price</span>
-                    <span>Remove</span>
-                    <hr />
-                    {cartItems.map((item) => (
-                        <CheckoutItem key={item.id} cartItem={item} />
-                    ))}
-                    <hr />
-                    <span className="total">TOTAL: {cartTotal}</span>
-                </div>
-            </div>
-        </>
-    );
+  const { cartItems, cartTotal } = useContext(CartContext);
+  return (
+    <>
+      <div className="container checkout-wrapper">
+        <div className="checkout-container">
+          <span className="checkout-item-title">Product</span>
+          <span className="checkout-item-title">Description</span>
+          <span className="checkout-item-title">Quantity</span>
+          <span className="checkout-item-title">Price</span>
+          <span className="checkout-item-title">Remove</span>
+          <hr />
+          {cartItems.map((item) => (
+            <CheckoutItem key={item.id} cartItem={item} />
+          ))}
+          <hr />
+          <span className="total">TOTAL: {cartTotal}</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cart;
